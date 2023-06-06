fe14649504435c1ca17ddf5bd904e3ecc9d9c480
august269 <akirui95@gmail.com>
2023-06-06 21:23:50 +0300

    add all features

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,32 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpenseFilter';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 
 const Expenses = (props) => {
+    const [expenses, setExpenses] = useState(props.items)
+
+    const filterChangeHandler = (selectedYear) => {
+        setExpenses(props.items.filter((expense) => {
+            return expense.date.getFullYear().toString() === selectedYear;
+        }));
+    };
+
     return (
         <Card className="expenses">
-            <ExpenseItem
-                title={props.items[0].title}
-                amount={props.items[0].amount}
-                date={props.items[0].date}
-            />
-            <ExpenseItem
-                title={props.items[1].title}
-                amount={props.items[1].amount}
-                date={props.items[1].date}
-            />
-            <ExpenseItem
-                title={props.items[2].title}
-                amount={props.items[2].amount}
-                date={props.items[2].date}
-            />
-            <ExpenseItem
-                title={props.items[3].title}
-                amount={props.items[3].amount}
-                date={props.items[3].date}
-            />
+            <ExpensesFilter onYearChanged={filterChangeHandler} />
+            {expenses.length === 0 && <h2>No expenses found</h2>}
+            <ExpensesChart expenses={expenses} />
+            {expenses.map((expense) => (
+                <ExpenseItem
+                    key={expense.id}
+                    title={expense.title}
+                    amount={expense.amount}
+                    date={expense.date} />
+            ))}
         </Card>
     );
 }
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+    const [flag, setFlag] = useState(false);
+
+    const flipFlag = () => {
+        console.log(flag);
+        setFlag(prevFlag => setFlag(!prevFlag));
+    }
+
     const saveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
+        flipFlag();
         props.onAddExpense(expenseData);
     };
 
     return (
         <div className='new-expense'>
-            <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+            {flag ? <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} flipFlag={flipFlag} />
+                : <button type='button' onClick={flipFlag}>Add New Expense</button>}
+
         </div>
     );
 };
